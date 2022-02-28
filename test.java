fdffab6fb765845ebaec88880539545c72277fa5
Balaram Ghosh <balaramghosh.net@gmail.com>
2022-02-28 16:47:15 +0530

    add hello.class and test.java

---
+++ test.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+class hello {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 2;
+
+        System.out.println(a + b);
+    }
+}
